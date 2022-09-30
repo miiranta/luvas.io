8a5d4e15d536ba482c6b4c883a516d60ed125e8f
m_iranta <lucasrezende12@hotmail.com>
2022-09-29 22:01:55 -0300

    Sessions / modules

--- src/run.js
+++ src/run.js
@@ -1,36 +1,44 @@
-const fs              = require('fs');
-const https           = require('https');
-const path            = require('path');
-const express         = require('express');
-const helmet          = require('helmet')
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const fs                    = require('fs');
+const https                 = require('https');
+const path                  = require('path');
+const express               = require('express');
+const helmet                = require('helmet')
+const cookieSession         = require("cookie-session")
+const redirectToHTTPS       = require('express-http-to-https').redirectToHTTPS
 
 const publicDirectory = path.join(__dirname, "../public") 
 const pagesDirectory = path.join(__dirname, "../pages")
 const sslDirectory = path.join(__dirname, "../ssl")
 
-const app = express();
-const httpsport = 443;
-const httpport  = 80;
-
-app.use(express.static(publicDirectory))
-app.use(redirectToHTTPS([], [], 301));
-app.use(helmet())
- 
-app.get('/*', (req, res) => {
-  res.sendFile(pagesDirectory + "/index.html")
-})
-
-var httpsOptions = {
+const httpsOptions = {
   key: fs.readFileSync(sslDirectory + '/privkey.pem'),
   cert: fs.readFileSync(sslDirectory + '/cert.pem'),
 };
 
+const exp = express();
+const server = https.createServer(httpsOptions, exp)
+const httpsport = process.env.PORT_HTTPS;
+const httpport  = process.env.PORT_HTTP;
+
+exp.use(express.static(publicDirectory))
+exp.use(redirectToHTTPS([], [], 301));
+exp.use(helmet())
+ 
+const session = cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_KEY_1, process.env.SESSION_KEY_1]
+})
+
+exp.get('/*', (req, res) => {
+  res.sendFile(pagesDirectory + "/index.html")
+})
+
+
 //Listen to http and redirect to https
-app.listen(httpport);
+exp.listen(httpport);
 
 //Listen to https
-https.createServer(httpsOptions, app).listen(httpsport, () => {
+server.listen(httpsport, () => {
   console.log(`Server is up on port ${httpsport}!`);
 });
 
