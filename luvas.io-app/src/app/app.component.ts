96a7de1f33b7289161df6f604f8e22ae0dc58709
Lucas Miranda <lucasrezende12@hotmail.com>
2024-12-28 23:24:33 -0300

    body simplebar

--- luvas.io-app/src/app/app.component.ts
+++ luvas.io-app/src/app/app.component.ts
@@ -5,12 +5,17 @@ import { GlassLoadingComponent } from '../glass/glass-loading/glass-loading.comp
 import { GlassBackgroundComponent } from '../glass/glass-background/glass-background.component';
 import { GlassNavbarComponent } from '../glass/glass-navbar/glass-navbar.component';
 
+import { SimplebarAngularModule, SimplebarAngularComponent } from 'simplebar-angular';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GlassLoadingComponent, GlassBackgroundComponent, GlassNavbarComponent],
+  imports: [RouterOutlet, GlassLoadingComponent, GlassBackgroundComponent, GlassNavbarComponent, SimplebarAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'luvas.io-app';
+
+  sbOptions = { autoHide: false, scrollbarMinSize: 100 };
+
 }
