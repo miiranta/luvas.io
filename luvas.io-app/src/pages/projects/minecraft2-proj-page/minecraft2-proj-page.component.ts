d5eb0d196bd35f112ab9383c305e2a454895e8d7
Lucas Miranda <lucasrezende12@hotmail.com>
2025-01-07 19:29:59 -0300

    star-t project page

--- luvas.io-app/src/pages/projects/minecraft2-proj-page/minecraft2-proj-page.component.ts
+++ luvas.io-app/src/pages/projects/minecraft2-proj-page/minecraft2-proj-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { GlassLoadingCloseOnViewInitComponent } from '../../../glass/glass-loading-close-on-view-init/glass-loading-close-on-view-init.component';
+import { GlassPresentationComponent } from '../../../glass/glass-presentation/glass-presentation.component';
+
 @Component({
   selector: 'minecraft2-proj-page',
-  imports: [],
+  imports: [GlassLoadingCloseOnViewInitComponent, GlassPresentationComponent],
   templateUrl: './minecraft2-proj-page.component.html',
   styleUrl: './minecraft2-proj-page.component.scss'
 })
--- luvas.io-app/src/pages/projects/start-proj-page/start-proj-page.component.ts
+++ luvas.io-app/src/pages/projects/start-proj-page/start-proj-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { GlassLoadingCloseOnViewInitComponent } from '../../../glass/glass-loading-close-on-view-init/glass-loading-close-on-view-init.component';
+import { GlassPresentationComponent } from '../../../glass/glass-presentation/glass-presentation.component';
+
 @Component({
   selector: 'start-proj-page',
-  imports: [],
+  imports: [GlassLoadingCloseOnViewInitComponent, GlassPresentationComponent],
   templateUrl: './start-proj-page.component.html',
   styleUrl: './start-proj-page.component.scss'
 })
