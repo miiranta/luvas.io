2f55f52be290cf5ae93ca1e4601fb364a94e19d9
Lucas Miranda <lucasrezende12@hotmail.com>
2025-07-13 16:50:11 -0300

    fix: double node server

--- personal-site/luvas.io-api/server.js
+++
@@ -1,9 +0,0 @@
-var app = require('./app.js');
-
-console.log('Starting server...');
-
-// Simple HTTP server - SSL handled by nginx proxy
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
